826a7112848be57f12dbeca78f853491c87c3e9a
Jean Ureña <juanpedro1519@gmail.com>
2016-12-26 21:06:54 -0300

    Part2 - Templating

--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 #importing the app variable of the app/__init__.py
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+	user = {'nickname' : "Jean"}
+	posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+
+	return render_template("index.html",
+							title='Home',
+							user=user,
+							posts=posts
+							)
