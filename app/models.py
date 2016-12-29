2c63400b2459b949d992b5c08dfa7045f76bf79d
Jean Ureña <juanpedro1519@gmail.com>
2016-12-29 15:51:14 -0400

    Part8 - Followers, Contacts and Friends

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 from app import db
 from hashlib import md5
 
+
+""" 
+Since this is an auxiliary table that has no data other than the foreign keys,
+we use the lower level APIs in flask-sqlalchemy to create the table without an associated model
+"""
+followers = db.Table('followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +22,13 @@ class User(db.Model):
 	posts = db.relationship("Post", backref="author", lazy="dynamic")
 	about_me = db.Column(db.String(40))
 	last_seen = db.Column(db.DateTime)
+
+	followed = db.relationship('User',
+								secondary=followers,
+								primaryjoin=(followers.c.follower_id == id),
+								secondaryjoin=(followers.c.followed_id == id),
+								backref=db.backref('followers', lazy='dynamic'),
+								lazy='dynamic')
 
 	# flask login needs this methos for handling authentication
 	@property
@@ -60,6 +77,26 @@ class User(db.Model):
 				break
 			version += 1
 		return new_nickname
+
+	# follow stuffs. its return an object and this object has to be commited
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+			return self
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+			return self
+
+	def is_following(self, user):
+		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		# return query object not the result
+		return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
+
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
--- app/views.py
+++ app/views.py
@@ -106,6 +106,9 @@ def signup():
 		user.authenticated = True
 		db.session.add(user)
 		db.session.commit()
+		# make the user follow to himself to see his own post
+		db.session.add(user.follow(user))
+		db.session.commit()
 		login_user(user)
 		return redirect(url_for('index'))
 	return render_template("signup.html",
@@ -144,3 +147,41 @@ def edit():
 		form.nickname.data = g.user.nickname
 		form.about_me.data = g.user.about_me
 	return render_template('edit.html', form=form)
+
+@app.route("/follow/<nickname>")
+@login_required
+def follow(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user is None :
+		flash('User %s not found' % nickname)
+		return redirect(url_for('index'))
+	if user == g.user :
+		flash("You can't Follow yourself!")
+		return redirect(url_for('user', nickname=nickname))
+	u = g.user.follow(user)
+	if u is None :
+		flash("Cannot follow " + nickname + ".")
+		return redirect(url_for('user', nickname=nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash("You are now following " + nickname + "!")
+	return redirect(url_for('user', nickname=nickname))
+
+@app.route("/unfollow/<nickname>")
+@login_required
+def unfollow(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user is None :
+		flash('User %s not found' % nickname)
+		return redirect(url_for('index'))
+	if user == g.user :
+		flash("You can't Follow yourself!")
+		return redirect(url_for('user', nickname=nickname))
+	u = g.user.unfollow(user)
+	if u is None :
+		flash("Cannot unfollow " + nickname + ".")
+		return redirect(url_for('user', nickname=nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash("You have stopped following " + nickname + "!")
+	return redirect(url_for('user', nickname=nickname))
